978e9cc2905d29a96c41eec0553593dea57c6a42
Tiago Fassoni <tiagofassoni@gmail.com>
2018-04-12 15:42:34 -0300

    Removes unused ipdb reference

--- boundaries/management/commands/import_countries_geojson.py
+++ boundaries/management/commands/import_countries_geojson.py
@@ -2,7 +2,6 @@ from django.core.management.base import BaseCommand
 from django.contrib.gis.geos import GEOSGeometry, MultiPolygon
 from boundaries.models import Country
 import json
-import ipdb
 
 
 class Command(BaseCommand):
